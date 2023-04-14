de9db0eae600f410c26244de36c29fe6d2025b3e
ummekulsum30 <127677855+ummekulsum30@users.noreply.github.com>
2023-04-14 22:28:40 +0530

    Add files via upload

---
+++ sess_auth.php
@@ -0,0 +1,15 @@
+<?php 
+
+if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') 
+    $link = "https"; 
+else
+    $link = "http"; 
+$link .= "://"; 
+$link .= $_SERVER['HTTP_HOST']; 
+$link .= $_SERVER['REQUEST_URI'];
+if(!isset($_SESSION['userdata']) && !strpos($link, 'login.php')){
+	redirect('login.php');
+}
+if(isset($_SESSION['userdata']) && strpos($link, 'login.php')){
+	redirect('index.php');
+}
---
+++ vnu-jar.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const childProcess = require('child_process')
+const vnu = require('vnu-jar')
+
+childProcess.exec('java -version', (error, stdout, stderr) => {
+  if (error) {
+    console.error('Skipping vnu-jar test; Java is missing.')
+    return
+  }
+
+  const is32bitJava = !/64-Bit/.test(stderr)
+
+
+  const ignores = [
+
+    'Attribute “autocomplete” is only allowed when the input type is.*'
+  ].join('|')
+
+  const args = [
+    '-jar',
+    vnu,
+    '--asciiquotes',
+    '--skip-non-html',
+
+    '--no-langdetect',
+    '--Werror',
+    `--filterpattern "${ignores}"`,
+    './*.html',
+    'docs_html/',
+    'pages/'
+  ]
+
+ 
+  if (is32bitJava) {
+    args.splice(0, 0, '-Xss512k')
+  }
+
+  return childProcess.spawn('java', args, {
+    shell: true,
+    stdio: 'inherit'
+  })
+    .on('exit', process.exit)
+})
